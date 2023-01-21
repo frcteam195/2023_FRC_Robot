2f710757c11d5c4326879fecd9127780df8f9633
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-01-21 06:27:08 +0000

    Update readme generator and readme

--- generate_readme.py
+++ generate_readme.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 from os.path import dirname, abspath
 import sys
 
 descriptionText='This is the main robot project. All nodes used in the robot should be defined in ros_projects.txt and all third party libraries should be defined in third_party_projects.txt. \
 In order to run this project, you need to have [ros_dev](https://github.com/frcteam195/ros_dev) at the same directory level as this repository. Then you can run `./ros_dev/mkrobot.sh clone` to pull all the node and third party libraries \
-and then run `./ros_dev/mkrobot.sh build` to build all the nodes. To deploy to the ROS robot, run `./ros_dev/mkrobot.sh deploy`.'
+and then run `./ros_dev/mkrobot.sh build` to build all the nodes. To deploy to the ROS robot, run `./ros_dev/mkrobot.sh deploy`. The naming standard for nodes is `[nameofnode]_[year]_node`.'
 if len(sys.argv) > 1:
     descriptionText=sys.argv[1]
 
